2bfe221f6687fa2827fddac422e132abaf1cd4c8
Vitrol <malachimart@gmail.com>
2013-12-10 10:56:17 -0600

    Made fixes to busRoutes.html and recentRoutes.html as well as resizing images

--- js/list-inlay.js
+++ js/list-inlay.js
@@ -2,15 +2,26 @@
  * list-inlay.js
  */
 var toggle = function(event){
-  event.currentTarget.parentNode.classList.toggle('expanded');
+    event.currentTarget.parentNode.classList.toggle('expanded');
+    hideAll();
 }
 var actions = document.querySelectorAll('.expand');
-for (var i = 0; i < actions.length; i++) {
-  actions[i].addEventListener('click', toggle, false)
+    for (var i = 0; i < actions.length; i++) {
+        actions[i].addEventListener('click', toggle, false)
 }
 
-/*function hideAll(event){
-    if (document.querySelectorAll('.profile.expanded') == true){
-        .profile.setAttribute('style', 'display:none;');
+function hideAll(){
+    if (document.querySelectorAll('.profile.expanded').length > 0){
+        var actions = document.querySelectorAll('.profile');
+        for (var i = 0; i < actions.length; i++) { 
+            if(actions[i].className != "profile expanded"){
+            actions[i].setAttribute('style', 'display:none;');
+            }    
+        }
+    }else {
+        var actions = document.querySelectorAll('.profile');
+        for (var i = 0; i < actions.length; i++) { 
+            actions[i].setAttribute('style', 'display:block;');
+        } 
     }
-}*/
+}
