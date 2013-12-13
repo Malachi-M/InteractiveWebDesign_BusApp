ff72c0a2eade8825a4b78130f70436b3d5d8cc49
Cid <cidcoto@Da-Pro.local>
2013-12-12 19:31:12 -0600

    Updates to map

--- js/mapJavaScript.js
+++ js/mapJavaScript.js
@@ -59,16 +59,30 @@ function busMarker(map, latlong, content, title){
 function initialize(coords){
     var googleLatLng = new google.maps.LatLng(coords.latitude, coords.longitude);
     var busLatLng = new google.maps.LatLng(40.76019790000001, -96.6537738);
+    var busLatLng1 = new google.maps.LatLng(40.85619790000001, -96.6337738);
+    var busLatLng2 = new google.maps.LatLng(40.79819790000001, -96.6937738);
+    var busLatLng3 = new google.maps.LatLng(40.82019790000001, -96.6337738);
     var mapOptions = {
         center: googleLatLng,
         zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        streetViewControl: false
+        
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
     
-    var content = "College View: Route 50";
-    var title = "Route 50 ";
+    var content = "College View: Route 50 Next Stop: 8 min";
+    var content1 = "Havelock: Route 41 Next Stop: 15 min";
+    var content2 = "West A: Route 51 Next Stop: 2 min";
+    var content3 = "Vine: Route 25 Next Stop: 20 min";
+    var title = "Route 50";
+    var title1 = "Route 41";
+    var title2 = "Route 51";
+    var title3 = "Route 25";
     marker(map, googleLatLng);
     busMarker(map, busLatLng, content, title);
+    busMarker(map, busLatLng1, content1, title1);
+    busMarker(map, busLatLng2, content2, title2);
+    busMarker(map, busLatLng3, content3, title3);
 }
